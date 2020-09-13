29eef3d3841021e4cc294dc42c5b2e986554ed29
oskaaditya <oska.aditya18@gmail.com>
2020-09-13 17:15:42 +0700

    tugas pertemuan 2

---
+++ VR Pertemuan 2/Assets/UbahWarna.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UbahWarna : MonoBehaviour
+{
+    public void Merah()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void Biru()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void Hitam()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+
+    public void Kuning()
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    public void Hijau()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void Putih()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    public void Magenta()
+    {
+        GetComponent<Renderer>().material.color = Color.magenta;
+    }
+
+    public void Cyan()
+    {
+        GetComponent<Renderer>().material.color = Color.cyan;
+    }
+
+    public void Grey()
+    {
+        GetComponent<Renderer>().material.color = Color.grey;
+    }
+}
